5c4f9a6d9d150909136bc0929009a661efea0d6c
ijefin <jeffersonmecmiler@hotmail.com>
2023-03-30 11:59:09 -0300

    finished login screen

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -14,20 +14,25 @@ export const Login = () => {
 
         <div className="login-form">
           <form>
-            <h1>Entrar</h1>
-            <label>
-              <span>Login</span>
-            </label>
-            <input
-              className="user"
-              placeholder="user@example.com"
-              type="text"
-            />
-            <label>
-              <span>Senha</span>
-            </label>
-            <input className="user" placeholder="*******" type="password" />
+            <h1 className="sign-in">Entrar</h1>
+            <div className="login-input">
+              <label>
+                <span className="login">Login</span>
+              </label>
+              <input
+                className="user"
+                placeholder="user@example.com"
+                type="text"
+              />
+            </div>
+            <div className="password-input">
+              <label>
+                <span className="login">Senha</span>
+              </label>
+              <input className="user" placeholder="*******" type="password" />
+            </div>
             <input className="submit" type="submit" />
+            <a href="">Ainda não possui conta? Cadastre-se!</a>
           </form>
         </div>
       </div>
