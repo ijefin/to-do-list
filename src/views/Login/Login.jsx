b970e635acab478d1612d02fc550619fec796306
ijefiN <jeffersonmecmiler@hotmail.com>
2023-09-11 12:19:13 -0300

    go

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import "./Login.css";
 import todo from "../../assets/login.png";
 
 export const Login = () => {
+  const [formData, setFormData] = useState({
+    nome: "",
+    email: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    // Simulação de lógica de envio
+    alert("Dados enviados com sucesso!");
+    // Limpar os campos do formulário
+    setFormData({
+      nome: "",
+      email: "",
+    });
+  };
+
   return (
     <>
       <div className="main-login-card">
@@ -14,16 +37,31 @@ export const Login = () => {
             <label className="label-text" htmlFor="login">
               Login
             </label>
-            <input type="text" id="login" name="login" placeholder="usuario123" required />
+            <input
+              type="text"
+              id="login"
+              name="login"
+              placeholder="usuario123"
+              onChange={handleChange}
+              required
+            />
           </div>
           <div>
             <label className="label-text" htmlFor="senha">
               Senha
             </label>
-            <input type="password" id="senha" name="senha" placeholder="*****" required />
+            <input
+              type="password"
+              id="senha"
+              name="senha"
+              placeholder="*****"
+              required
+            />
           </div>
           <div>
-            <button type="submit">Entrar</button>
+            <button type="submit" onClick={handleSubmit}>
+              Entrar
+            </button>
           </div>
         </div>
       </div>
